8faa2af691d5988bfc4eff147db20fe96e57f080
Vithanala <rambabu.vithanala.ext@nokia.com>
2020-07-26 13:09:34 +0530

    files areupdated

--- src/test/java/TestNG/addressbook.java
+++ src/test/java/TestNG/addressbook.java
@@ -1,37 +1,68 @@
 package TestNG;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class addressbook {
-	WebDriver driver;
-	@BeforeMethod
-	public void launch() {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		driver = new ChromeDriver();
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		
+		ChromeOptions options = new ChromeOptions();
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		
+		prefs.put("credentials_enable_service", false);
+		prefs.put("profile.password_manager_enabled", false);
+		prefs.put("disable_infobars", true);		
+		prefs.put("profile.default_content_setting_values.notifications", 2);
+		options.setExperimentalOption("prefs", prefs);
+		options.addArguments("disable-infobars");
+		options.addArguments("--start-maximized");
+		DesiredCapabilities cap=DesiredCapabilities.chrome();
+		cap.setCapability(ChromeOptions.CAPABILITY, options);
+		
+		WebDriver driver=new ChromeDriver(cap);
+		
+		//driver.get("http://www.facebook.com");
+		
+		By loginButtonLocator = By.xpath("//input[@id='Login']");
+		By passwordLocator = By.xpath("//input[@id='password']");
+		By usernameLocator = By.xpath("//input[@id='username']");
+		By setup=By.xpath("//div[@class='slds-global-header__item']/ul/li[7]"); 
+		 
 		driver.manage().window().maximize();
-	    driver.get("http://localhost:8080/addressbook/");
-		}
-   @Test
-  public void verifyaddbook() {
-	   driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-	   driver.findElement(By.xpath("//div[@class='v-button v-widget']")).click();
-	   driver.findElement(By.id("gwt-uid-5")).sendKeys("Testing");
-	   driver.findElement(By.id("gwt-uid-7")).sendKeys("Selenium");
-	   driver.findElement(By.id("gwt-uid-9")).sendKeys("9849087272");
-	   driver.findElement(By.id("gwt-uid-11")).sendKeys("Selenium@gmail.com");
-	   driver.findElement(By.id("gwt-uid-13")).sendKeys("01/11/1985");
-	   driver.findElement(By.xpath("//div[@class='v-button v-widget primary v-button-primary']")).click();
-   }
-   @AfterMethod
-   public void close() {
-   }
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		 
+		//driver.get("https://www.freecrm.com");
+		driver.get("https://nokia--qcrm3.lightning.force.com/");
+		driver.findElement(usernameLocator).sendKeys("ch_qcrm3_sys@nokia.com.qcrm3");
+		driver.findElement(passwordLocator).sendKeys("QCRM3@1234");
+		driver.findElement(loginButtonLocator).click();
+		Thread.sleep(5000);
+		driver.findElement(setup).click();	
+		Thread.sleep(2000);
+		
+	Actions act=new Actions(driver);
+		act.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+		
+		
+
+	}
+
 }
 
    
---
+++ src/test/java/com/nokia/Utility.java
@@ -0,0 +1,35 @@
+package com.nokia;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+
+public class Utility 
+{
+ 
+	public static String getScreenshot(WebDriver driver)
+	{
+		TakesScreenshot ts=(TakesScreenshot) driver;
+		
+		File src=ts.getScreenshotAs(OutputType.FILE);
+		
+		String path=System.getProperty("user.dir")+"/Screenshot/"+System.currentTimeMillis()+".png";
+		
+		File destination=new File(path);
+		
+		try 
+		{
+			FileUtils.copyFile(src, destination);
+		} catch (IOException e) 
+		{
+			System.out.println("Capture Failed "+e.getMessage());
+		}
+		
+		return path;
+	}
+	
+}
